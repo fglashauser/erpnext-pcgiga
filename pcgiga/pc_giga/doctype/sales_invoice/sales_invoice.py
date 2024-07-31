e05be27a5dec618638e6f3d5c2709b0562cb8dc7
Florian Glashauser <florian.glashauser@pc-giga.de>
2024-07-31 07:34:52 +0000

    Option -nach Rechnung schliessen- zu Sales Order hinzugefügt

--- pcgiga/pc_giga/doctype/sales_invoice/sales_invoice.py
+++ pcgiga/pc_giga/doctype/sales_invoice/sales_invoice.py
@@ -1,12 +1,15 @@
 import frappe
 
 def on_submit(doc, method):
+    """
+    Close Sales Order when Sales Invoice is submitted and the option "Nach Rechnung schließen" is checked
+    """
     if doc.docstatus == 1:
         sales_orders = doc.items
         for item in sales_orders:
             if item.sales_order:
                 sales_order = frappe.get_doc("Sales Order", item.sales_order)
-                if sales_order.status != 'Closed':
+                if sales_order.status != 'Closed' and sales_order.autoclose_after_invoice == 1:
                     sales_order.update_status('Closed')
                     sales_order.save()
                     print('Sales Order {0} has been closed'.format(sales_order.name))
